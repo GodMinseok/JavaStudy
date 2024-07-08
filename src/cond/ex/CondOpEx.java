372034a4823b33bc69f7eae415ac50248dae1448
GodMinseok <mseok830@naver.com>
2024-07-08 22:28:24 +0900

    Section 4. 조건문 (문제와 풀이2)

---
+++ src/cond/ex/CondOpEx.java
@@ -0,0 +1,14 @@
+package cond.ex;
+
+public class CondOpEx {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int Max = (a > b)? a : b;
+
+        System.out.println("더 큰 숫자는" + Max + "입니다.");
+    }
+}
